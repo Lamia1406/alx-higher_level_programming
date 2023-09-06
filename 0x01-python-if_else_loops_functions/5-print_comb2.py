8b251eed17cd2ca32c1dc4d719a3a2cb29bb3338
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-09-05 19:33:57 -0700

    print numbers from 9 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for num in range(0, 100):
     if num < 10:
-        print("0", end='')
+        num = f"{num:02d}"
     if num == 99:
         print("{}".format(num))
     else:
