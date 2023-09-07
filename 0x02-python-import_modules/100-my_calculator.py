85c86ccc75692a98b268374b4015bf5594a4776c
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-09-07 10:53:17 -0700

    import all math functions and handle basic operations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    if len(sys.argv) == 1:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if len(sys.argv) == 4:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] == "+":
+            result = add(a, b)
+        elif sys.argv[2] == "-":
+            result = sub(a, b)
+        elif sys.argv[2] == "*":
+            result = mul(a, b)
+        elif sys.argv[2] == "/":
+            result = div(a, b)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+        print("{} {} {} = {}".format(a, sys.argv[2], b, result))
+        sys.exit(0)
