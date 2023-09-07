34c0a5c4b10bf124e543102caff2ed31bd4b7121
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-09-07 09:10:55 -0700

    print the number of and the list of a program's arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    args = len(sys.argv) - 1
+    print("{} argument".format(args), end='')
+    if args == 0 or args > 1:
+        print("s")
+    elif args == 1:
+        print()
+    if args >= 1:
+        for i in range(1, args + 1):
+            print("{}: {}".format(i, sys.argv[i]))
