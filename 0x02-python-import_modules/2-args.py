560ca832bd7ea9d3236e76c6a1b595b8474a6109
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-09-09 13:56:44 -0700

    fix output in task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,12 @@ if __name__ == "__main__":
     import sys
     args = len(sys.argv) - 1
     print("{} argument".format(args), end='')
-    if args == 0 or args > 1:
+    if args > 1:
         print("s:")
     elif args == 1:
         print(":")
+    elif args == 0:
+        print("s.")
     if args >= 1:
         for i in range(1, args + 1):
             print("{}: {}".format(i, sys.argv[i]))
