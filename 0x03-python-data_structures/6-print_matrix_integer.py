88d630e57bd2b3ac8847b737a64064c726b71fac
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-09-15 00:07:24 -0700

    print all elements in matrix of integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for ligne in matrix:
-        c = 0
-        for cell in ligne:
-            if c < len(ligne) - 1:
-                print("{:d}".format(cell), end=' ')
-            else:
-                print("{:d}".format(cell))
-            c += 1
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        print()
+    else:
+        for ligne in matrix:
+            c = 0
+            for cell in ligne:
+                if c < len(ligne) - 1:
+                    print("{:d}".format(cell), end=' ')
+                else:
+                    print("{:d}".format(cell))
+                    c += 1
