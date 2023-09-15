6f730a1fa7f87e76b90cb6dd5b821c235e9594be
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-09-14 22:29:30 -0700

    implement new test cases

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * insert_node - inserts a node into a sorted linked list
  * @head: Pointer to a pointer to the head of the list
@@ -16,8 +16,31 @@ listint_t *insert_node(listint_t **head, int number)
 	if (node == NULL)
 		return (NULL);
 	node->n = number;
-	while (temp_head && temp_head->next->n < number)
-		temp_head = temp_head->next;
+	if (*head == NULL)
+	{
+		node->next = NULL;
+		*head = node;
+		return (node);
+	}
+	if ((*head)->next->n > number)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+	while (temp_head)
+	{
+		if (temp_head->next == NULL)
+		{
+			node->next = NULL;
+			temp_head->next = node;
+			return (node);
+		}
+		else if (temp_head->next->n < number)
+			temp_head = temp_head->next;
+		else
+			break;
+}
 	node->next = temp_head->next;
 	temp_head->next = node;
 	return (node);
