501934dbd24fd4db791a24cecf37b6a868b4d90a
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-09-16 19:28:17 -0700

    convert roman numerals to integers

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+def roman_to_int(roman_string):
+    conversion_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+    num = 0
+    i = 0
+    str_len = len(roman_string)
+    while i < str_len:
+        current_digit = conversion_dict[roman_string[i]]
+        if i + 1 < str_len:
+            next_digit = conversion_dict[roman_string[i + 1]]
+            if current_digit < next_digit:
+                num += next_digit - current_digit
+                i += 1
+            else:
+                num += conversion_dict[roman_string[i]]
+        else:
+            num += conversion_dict[roman_string[i]]
+        i += 1
+    return num
