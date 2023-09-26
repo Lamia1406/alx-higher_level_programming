116a6f58f532433b97bf46c536551c9cf8e5dd5e
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-09-26 11:03:31 -0700

    write a class Suare with a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""This module contains a class named Square """
+
+
+class Square:
+    """ this class defines a square
+    Args:
+        size: Private instance attribute defining the size of a square
+    """
+    def __init__(self, size):
+        self.__size = size
