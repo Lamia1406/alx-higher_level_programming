438597d0fcdbf2e64423e3006b6aaffd6e4285c0
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-09-27 07:56:19 -0700

    add a method that prints in stdout the square with the character # to Square class

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""This module contains a class named Square """
+
+
+class Square:
+    """ this class defines a square
+    Args:
+        size: Private instance attribute defining the size of a square
+                that is of int type and absolutely positive
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """The area method returns the current square area """
+        return self.__size ** 2
+
+    def my_print(self):
+        """ the method prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end='')
+            print()
+
+    @property
+    def size(self):
+        """ size getter : to retrieve it """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ size setter : to set it
+        Args:
+            value : the size value (should be an int)
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
