2153ca81e2942475a7d64cade7e3041d3c7002ca
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-10-09 12:50:07 -0700

    add a public instance class area() to the class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this module consists only of one class: BaseGeometry"""
+
+
+class BaseGeometry:
+    """an class with one instance method"""
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
