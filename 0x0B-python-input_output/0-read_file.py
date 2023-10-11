1c89179a4970943cd83ef28c0200851882e0c577
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-10-11 04:58:26 -0700

    read a text file (UTF8) and prints it to stdout:

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+"""This module contains only one function: read_file(filename="")"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
+    f.closed
