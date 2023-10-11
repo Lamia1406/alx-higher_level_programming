b68f5f2c2598eb589edac3082143d0e5e75d5893
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-10-11 15:01:06 -0700

    fix psycode error

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" this module consists only of one function: is_kind_of_class(obj, a_class)"""
+""" this module consists only of one function:
+            is_kind_of_class(obj, a_class)"""
 
 
 def is_kind_of_class(obj, a_class):
