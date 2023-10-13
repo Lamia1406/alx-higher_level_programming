f182b9096e3cf6c25aa8977af53a6c92441e3078
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-10-13 02:07:33 -0700

    fix bugs

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -14,16 +14,16 @@ class Rectangle:
             width:  defining the width of the rectangle
             height: defining the height of the rectangle
         """
-        if isinstance(height, int) is False:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = height
         if isinstance(width, int) is False:
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
         self.__width = width
+        if isinstance(height, int) is False:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
         Rectangle.number_of_instances += 1
 
     @property
