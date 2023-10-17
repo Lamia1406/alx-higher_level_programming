5f392f773209b0459dff71749383add1f111f60c
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-10-16 23:48:47 -0700

    fix error when width or height are equal to 0 in rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -38,7 +38,8 @@ class Rectangle(Base):
         """setter of the width of a rectangle"""
         if type(width) != int:
             raise TypeError("width must be an integer")
-        if width < 0:
+        print("hi", width)
+        if width <= 0:
             raise ValueError("width must be > 0")
         self.__width = width
 
@@ -52,7 +53,7 @@ class Rectangle(Base):
         """setter of the height of a rectangle"""
         if type(height) != int:
             raise TypeError("height must be an integer")
-        if height < 0:
+        if height <= 0:
             raise ValueError("height must be > 0")
         self.__height = height
 
