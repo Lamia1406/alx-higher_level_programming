68c482f580668e75a56622f65bc281d84add53e1
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-10-29 16:03:59 -0700

    print a text with 2 new lines after each of these characters: ., ? and :

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module supplies one function, text_indentation(text)"""
+
+
+def text_indentation(text):
+    """prints a text with 2 new lines
+        after each of these characters: ., ? and :"""
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    for i in range(len(text)):
+        if text[i - 1] == "." or text[i - 1] == "?" or text[i - 1] == ":":
+            if text[i] == " ":
+                continue
+        print(text[i], end="")
+        if text[i] == "." or text[i] == "?" or text[i] == ":":
+            print("\n")
