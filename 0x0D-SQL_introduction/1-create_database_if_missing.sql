57c954f6253253b942c43ce2785b5e22e3b9d324
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-11-14 02:04:55 -0800

    create a database in the server

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- creates the database hbtn_0c_0 in your MySQL server
+-- create a new database in MySQL, IF NOT EXISTS prevents an error from occurring if the database already exists.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
