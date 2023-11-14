d8c2b63e0e01531e780661d4cbcb11f6ec814370
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-11-14 07:52:42 -0800

    list name, score from all rows of a tables ordered by score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- lists score, name from all rows of the table second_table from the database hbtn_0c_0 in your MySQL server ordered by score
+SELECT score, name FROM second_table ORDER BY score desc;
