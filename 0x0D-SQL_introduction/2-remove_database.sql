26337cdd3d1ac0d4a30e1cc4c33e4ab9e4a361b2
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-11-14 02:16:34 -0800

    delete a database from the server

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- deletes the database hbtn_0c_0 in your MySQL server
+-- deletes a database in MySQL, IF EXISTS prevents an error from occurring if the database doesn't exist
+DROP DATABASE IF EXISTS hbtn_0c_0;
