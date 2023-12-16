f9a0d3cc01ac3cffda9df6001ed841abe1df2835
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-12-16 11:14:25 -0800

    correct query

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM cities ORDER BY id")
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+                   FROM cities, states WHERE cities.state_id = states.id\
+                   ORDER BY id")
     for row in cursor.fetchall():
         print(row)
     cursor.close()
