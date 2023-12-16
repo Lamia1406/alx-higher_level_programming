d6f7d32ae02766bc546c55a97f9daac7af91c5dc
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-12-16 10:32:25 -0800

    list all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This script lists all states ordered by id in ascending order"""
+
+import MySQLdb
+import sys
+
+
+db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                     passwd=sys.argv[2], db=sys.argv[3], port=3306)
+cursor = db.cursor()
+cursor.execute("SELECT * FROM states ORDER BY id")
+for row in cursor.fetchall():
+    print(row)
+cursor.close()
+db.close()
