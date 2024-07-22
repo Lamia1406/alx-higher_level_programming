429b596ca3c2a788d55fc5b4816d896cc5fcdcfb
lamia hamdi <lamia.hamdi@univ-constantine2.dz>
2024-07-22 16:13:46 -0700

    solve the problem

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+""" find the peak in list of integers"""
+
+
 def find_peak(list_of_integers):
+    """find the peak in a list of integers """
     if not list_of_integers:
         return None
+    elif len(list_of_integers) == 1:
+        return list_of_integers[0]
+    else:
+        peaks = []
+        for i in range(len(list_of_integers) - 1):
+            peak = list_of_integers[i]
+            successor = list_of_integers[i + 1]
+            ancestor = list_of_integers[i - 1]
+            if peak >= successor and peak >= ancestor:
+                peaks.append(peak)
+        return find_peak(peaks)
